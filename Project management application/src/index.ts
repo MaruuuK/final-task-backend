f1b41a85c9ab49f6f928678eb5bcc496581d4d2f
Mariia Kuryleva <maru.kuryleva@gmail.com>
2023-11-02 10:28:00 +0400

    feat: add data

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,21 +3,20 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
-
 (async () => {
-  try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
-    serverService.server.listen(process.env.PORT || PORT, function () {
-      console.log('Сервер ожидает подключения...');
-    })
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await mongoose.connect(
+            'mongodb+srv://maruuuk:IzNntnxHLZ0kJ0nB@atlascluster.jfkytgk.mongodb.net/?retryWrites=true&w=majority'
+        );
+        serverService.server.listen(process.env.PORT || PORT, function () {
+            console.log('Сервер ожидает подключения...');
+        });
+    } catch (error) {
+        console.log(error);
+    }
 })();
 
-
-
 process.on('SIGINT', async () => {
-  await mongoose.disconnect();
-  process.exit();
+    await mongoose.disconnect();
+    process.exit();
 });
